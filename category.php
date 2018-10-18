bc4469b27e5427a34436e4d74c05955c6734762d
Viktoriya <kapitonenko.vick@ya.ru>
2018-10-18 20:37:41 +0300

    Страница категории

---
+++ category.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+
+<div class="wrap">
+    <div class="container">
+      <div class="content row">
+        <div class="col-sm-12 posts col-md-7 col-md-offset-1">
+            <h1 class="blog-breadcrumb">Главная / <span class="active"><?php the_title(); ?></span> /</h1>
+            <?php
+            if (have_posts()):
+              echo '<div id="ajax">';
+              while (have_posts()): the_post();
+                get_template_part('template-parts/post');
+              endwhile;
+              echo '</div>';
+            endif; load_more_button(); ?>
+        </div>
+
+        <?php get_sidebar(); ?>
+      </div>
+    </div>
+  </div>
+  <!-- End Content Wrapper-->
+
+<?php get_footer(); ?>
